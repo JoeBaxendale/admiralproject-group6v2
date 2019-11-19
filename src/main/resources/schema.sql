1bdbf97a7debec0c9bd4935421fcf0f867683532
Daniel Addis <AddisD@cardiff.ac.uk>
2019-11-19 13:25:19 +0000

    DB schema, all created it with mob development

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,81 @@
+SET MODE MySQL;
+SET IGNORECASE = TRUE;
+
+-- -----------------------------------------------------
+-- Table `admiral_staff`
+-- -----------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `admiral_staff`
+(
+    `admiral_staff_id`              INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `admiral_staff_email`           VARCHAR(45)  NOT NULL,
+    `password`                      VARCHAR(45)  NOT NULL,
+    `first_name`                    VARCHAR(45)  NOT NULL,
+    `last_name`                     VARCHAR(45)  NOT NULL,
+
+    PRIMARY KEY (`id`)
+)
+    ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `managers`
+-- -----------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `managers`
+(
+    `manager_id`                   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `manager_email`                VARCHAR(45)  NOT NULL,
+    `password`                      VARCHAR(45)  NOT NULL,
+    `first_name`                    VARCHAR(45)  NOT NULL,
+    `last_name`                     VARCHAR(45)  NOT NULL,
+    `status`                        BIT  NOT NULL,
+
+
+    PRIMARY KEY (`managers_id`)
+)
+    ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `contractors`
+-- -----------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `contractors`
+(
+    `contractor_id`                   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `contractor_email`                VARCHAR(45)  NOT NULL,
+    `password`                      VARCHAR(45)  NOT NULL,
+    `first_name`                    VARCHAR(45)  NOT NULL,
+    `last_name`                     VARCHAR(45)  NOT NULL,
+    `managers_id`                   INT  NOT NULL,
+    `status`                        BIT  NOT NULL,
+
+
+    PRIMARY KEY (`contractor_id`)
+    FOREIGN KEY (`managers_id`) REFERENCES managers (`managers_id`)
+
+)
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `time_sheets`
+-- -----------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `timesheet`
+(
+    `timesheet_id`                   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `contractor_id`                  INT  NOT NULL,
+    `number_days`                    INT NOT NULL,
+    `overtime`                       INT  NOT NULL,
+    `start_date`                     DATE  NOT NULL,
+    `end_date`                       DATE  NOT NULL,
+    `date_submitted`                 DATE  NOT NULL,
+    `notes`                          VARCHAR(100),
+    `status`                         VARCHAR(25) NOT NULL,
+
+
+    PRIMARY KEY (`timesheet_id`)
+    FOREIGN KEY (`contractor_id`) REFERENCES contractors (`contractor_id`)
+
+)
+    ENGINE = InnoDB;
