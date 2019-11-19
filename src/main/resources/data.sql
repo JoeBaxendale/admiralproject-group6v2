5f61e9bd5642910ff6e91fdd44e19e6b919fcd25
Dexter Gordon <GordonD2@cardiff.ac.uk>
2019-11-19 13:55:23 +0000

    Manager Example Data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
+
+/* ADMIRAL STAFF DATA */
+
 insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (1, 'victoria@hotmail.com', 'victoria123', 'Victoria', 'Smith');
 insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (2, 'adams@hotmail.com', 'adams111', 'Adams', 'Court');
 insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (3, 'fernando@gmail.com', 'fernando122', 'Fernando', 'Barbosa');
@@ -13,3 +16,14 @@ insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, firs
 insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (13, 'avery@gmail.com', 'p123', 'Avery', 'Corbyn');
 insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (14, 'maddison@hotmail.com', 'p123', 'Maddison', 'Wilson');
 insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (15, 'riley@gmail.com', 'riley777', 'Riley', 'Brown');
+
+
+/* MANAGER DATA */
+
+insert into managers (manager_id, manager_email, password, first_name, last_name, status) values (1, 'dave@gmail.com', 'dave12', 'Dave', 'Harris', 1);
+insert into managers (manager_id, manager_email, password, first_name, last_name, status) values (2, 'joe@gmail.com', 'joe51', 'Joe', 'Young', 0);
+insert into managers (manager_id, manager_email, password, first_name, last_name, status) values (3, 'dan@outlook.com', 'dan123', 'Dan', 'Brown', 1);
+insert into managers (manager_id, manager_email, password, first_name, last_name, status) values (4, 'matthew@hotmail.co.uk', 'matthew423!', 'Matthew', 'Mort', 1);
+insert into managers (manager_id, manager_email, password, first_name, last_name, status) values (5, 'james@yahoo.com', 'james#312', 'James', 'Smith', 0);
+insert into managers (manager_id, manager_email, password, first_name, last_name, status) values (6, 'kyle@gmail.co.uk', 'kyle1', 'Kyle', 'Adams', 0);
+
