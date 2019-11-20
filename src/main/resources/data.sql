e25aa59ac8a40addda81d78c7bdc5a4ee737889a
Karla Mendivelso Espinosa <MendivelsoEspinosaKN@cardiff.ac.uk>
2019-11-20 13:53:55 +0000

    Edited schema and data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,43 +1,56 @@
+/* ADMIRAL USER DATA */
+insert into admiral_user (admiral_user_id, first_name, last_name, admiral_staff_email, password, status) values (1,'Victoria', 'Smith', 'victoria@hotmail.com', 'victoria123', 'true');
+
+/* ADMIRAL ROLE DATA */
+insert into admiral_role (admiral_role_id, admiral_role_name, admiral_role_description) values (1, 'Manager', 'This user has ultimate rights for everything')
+insert into admiral_role (admiral_role_id, admiral_role_name, admiral_role_description) values (2, 'Admin', 'This user has admin rights for administrative work')
+insert into admiral_role (admiral_role_id, admiral_role_name, admiral_role_description) values (3, 'Contractor', 'This user has access to site, after login')
+
+/* ADMIRAL USER_ROLE DATA */
+insert into admiral_user_role (admiral_user_id, admiral_role_id) values (1, 1)
+insert into admiral_user_role (admiral_user_id, admiral_role_id) values (1, 2)
+insert into admiral_user_role (admiral_user_id, admiral_role_id) values (1, 3)
+
 
 /* ADMIRAL STAFF DATA */
-
-insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (1, 'victoria@hotmail.com', 'victoria123', 'Victoria', 'Smith');
-insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (2, 'adams@hotmail.com', 'adams111', 'Adams', 'Court');
-insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (3, 'fernando@gmail.com', 'fernando122', 'Fernando', 'Barbosa');
-insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (4, 'jhon@hotmail.com', 'jhon333', 'Jhon', 'Barco');
-insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (5, 'amelia@hotmail.com', 'amelia654', 'Amelia', 'Brown');
-insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (6, 'stiles@gmail.com', 'stiles987', 'Stiles', 'Cooper');
-insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (7, 'allison@hotmail.com', 'allison223', 'Allison', 'Argent');
-insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (8, 'scot@hotmail.com', 'scot222', 'Scot', 'Stanesby');
-insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (9, 'josseth@gmail.com', 'josseth299', 'Josseth', 'Clarck');
-insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (10, 'julian@hotmail.com', 'julian000', 'Julian', 'Roberts');
-insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (11, 'jackson@gmail.com', 'jackson111', 'Jackson', 'Jones');
-insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (12, 'harper@hotmail.com', 'p123', 'Harper', 'Lawson');
-insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (13, 'avery@gmail.com', 'p123', 'Avery', 'Corbyn');
-insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (14, 'maddison@hotmail.com', 'p123', 'Maddison', 'Wilson');
-insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (15, 'riley@gmail.com', 'riley777', 'Riley', 'Brown');
-
-/*MANAGER DATA*/
-
-insert into managers (manager_id, manager_email, password, first_name, last_name, status) values (1, 'dave@gmail.com', 'dave12', 'Dave', 'Harris', 1);
-insert into managers (manager_id, manager_email, password, first_name, last_name, status) values (2, 'joe@gmail.com', 'joe51', 'Joe', 'Young', 1);
-insert into managers (manager_id, manager_email, password, first_name, last_name, status) values (3, 'dan@outlook.com', 'dan123', 'Dan', 'Brown', 1);
-insert into managers (manager_id, manager_email, password, first_name, last_name, status) values (4, 'matthew@hotmail.co.uk', 'matthew423!', 'Matthew', 'Mort', 1);
-insert into managers (manager_id, manager_email, password, first_name, last_name, status) values (5, 'james@yahoo.com', 'james#312', 'James', 'Smith', 1);
-insert into managers (manager_id, manager_email, password, first_name, last_name, status) values (6, 'kyle@gmail.co.uk', 'kyle1', 'Kyle', 'Adams', 0);
-
-
-/* CONTRACTOR DATA */
-
-insert into contractors (contractor_id, contractor_email, password, first_name, last_name, managers_id, status) values (1, 'Peter@gmail.co.uk', 'kPeter1', 'Peter', 'Bridge', 1, 1);
-insert into contractors (contractor_id, contractor_email, password, first_name, last_name, managers_id, status) values (2, 'Jason@gmail.co.uk', 'kPJason1', 'Jason', 'Bridger', 1, 1);
-insert into contractors (contractor_id, contractor_email, password, first_name, last_name, managers_id, status) values (3, 'Zach@gmail.co.uk', 'kZach1', 'Zach', 'matters', 2, 1);
-insert into contractors (contractor_id, contractor_email, password, first_name, last_name, managers_id, status) values (4, 'Sam@gmail.co.uk', 'kSam1#', 'Sam', 'brokes', 3, 1);
-insert into contractors (contractor_id, contractor_email, password, first_name, last_name, managers_id, status) values (5, 'Euan@gmail.co.uk', 'Euan98', 'Euan', 'Evans', 4, 1);
-insert into contractors (contractor_id, contractor_email, password, first_name, last_name, managers_id, status) values (6, 'Finn@gmail.co.uk', 'Finn8', 'Finn', 'Tomson', 4, 1);
-insert into contractors (contractor_id, contractor_email, password, first_name, last_name, managers_id, status) values (7, 'Bill@gmail.co.uk', 'Bill3', 'Bill', 'Gates', 4, 1);
-insert into contractors (contractor_id, contractor_email, password, first_name, last_name, managers_id, status) values (8, 'Mark@gmail.co.uk', 'Mark5', 'Mark', 'Zuck', 4, 1);
-insert into contractors (contractor_id, contractor_email, password, first_name, last_name, managers_id, status) values (9, 'Mike@gmail.co.uk', 'Mike73', 'Mike', 'Jones', 4, 1);
-insert into contractors (contractor_id, contractor_email, password, first_name, last_name, managers_id, status) values (10, 'Jane@gmail.co.uk', 'Jane72!', 'Jane', 'Smith', 5, 0);
-insert into contractors (contractor_id, contractor_email, password, first_name, last_name, managers_id, status) values (11, 'Sarah@gmail.co.uk', 'Shar333', 'Sarah', 'Smith', 5, 1);
-insert into contractors (contractor_id, contractor_email, password, first_name, last_name, managers_id, status) values (12, 'Mike@gmail.co.uk', 'Mike62626', 'Mike', 'Zuch', 5, 1);
+--
+-- insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (1, 'victoria@hotmail.com', 'victoria123', 'Victoria', 'Smith');
+-- insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (2, 'adams@hotmail.com', 'adams111', 'Adams', 'Court');
+-- insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (3, 'fernando@gmail.com', 'fernando122', 'Fernando', 'Barbosa');
+-- insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (4, 'jhon@hotmail.com', 'jhon333', 'Jhon', 'Barco');
+-- insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (5, 'amelia@hotmail.com', 'amelia654', 'Amelia', 'Brown');
+-- insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (6, 'stiles@gmail.com', 'stiles987', 'Stiles', 'Cooper');
+-- insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (7, 'allison@hotmail.com', 'allison223', 'Allison', 'Argent');
+-- insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (8, 'scot@hotmail.com', 'scot222', 'Scot', 'Stanesby');
+-- insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (9, 'josseth@gmail.com', 'josseth299', 'Josseth', 'Clarck');
+-- insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (10, 'julian@hotmail.com', 'julian000', 'Julian', 'Roberts');
+-- insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (11, 'jackson@gmail.com', 'jackson111', 'Jackson', 'Jones');
+-- insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (12, 'harper@hotmail.com', 'p123', 'Harper', 'Lawson');
+-- insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (13, 'avery@gmail.com', 'p123', 'Avery', 'Corbyn');
+-- insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (14, 'maddison@hotmail.com', 'p123', 'Maddison', 'Wilson');
+-- insert into admiral_staff (admiral_staff_id, admiral_staff_email, password, first_name, last_name) values (15, 'riley@gmail.com', 'riley777', 'Riley', 'Brown');
+--
+-- /*MANAGER DATA*/
+--
+-- insert into managers (manager_id, manager_email, password, first_name, last_name, status) values (1, 'dave@gmail.com', 'dave12', 'Dave', 'Harris', 1);
+-- insert into managers (manager_id, manager_email, password, first_name, last_name, status) values (2, 'joe@gmail.com', 'joe51', 'Joe', 'Young', 1);
+-- insert into managers (manager_id, manager_email, password, first_name, last_name, status) values (3, 'dan@outlook.com', 'dan123', 'Dan', 'Brown', 1);
+-- insert into managers (manager_id, manager_email, password, first_name, last_name, status) values (4, 'matthew@hotmail.co.uk', 'matthew423!', 'Matthew', 'Mort', 1);
+-- insert into managers (manager_id, manager_email, password, first_name, last_name, status) values (5, 'james@yahoo.com', 'james#312', 'James', 'Smith', 1);
+-- insert into managers (manager_id, manager_email, password, first_name, last_name, status) values (6, 'kyle@gmail.co.uk', 'kyle1', 'Kyle', 'Adams', 0);
+--
+--
+-- /* CONTRACTOR DATA */
+--
+-- insert into contractors (contractor_id, contractor_email, password, first_name, last_name, managers_id, status) values (1, 'Peter@gmail.co.uk', 'kPeter1', 'Peter', 'Bridge', 1, 1);
+-- insert into contractors (contractor_id, contractor_email, password, first_name, last_name, managers_id, status) values (2, 'Jason@gmail.co.uk', 'kPJason1', 'Jason', 'Bridger', 1, 1);
+-- insert into contractors (contractor_id, contractor_email, password, first_name, last_name, managers_id, status) values (3, 'Zach@gmail.co.uk', 'kZach1', 'Zach', 'matters', 2, 1);
+-- insert into contractors (contractor_id, contractor_email, password, first_name, last_name, managers_id, status) values (4, 'Sam@gmail.co.uk', 'kSam1#', 'Sam', 'brokes', 3, 1);
+-- insert into contractors (contractor_id, contractor_email, password, first_name, last_name, managers_id, status) values (5, 'Euan@gmail.co.uk', 'Euan98', 'Euan', 'Evans', 4, 1);
+-- insert into contractors (contractor_id, contractor_email, password, first_name, last_name, managers_id, status) values (6, 'Finn@gmail.co.uk', 'Finn8', 'Finn', 'Tomson', 4, 1);
+-- insert into contractors (contractor_id, contractor_email, password, first_name, last_name, managers_id, status) values (7, 'Bill@gmail.co.uk', 'Bill3', 'Bill', 'Gates', 4, 1);
+-- insert into contractors (contractor_id, contractor_email, password, first_name, last_name, managers_id, status) values (8, 'Mark@gmail.co.uk', 'Mark5', 'Mark', 'Zuck', 4, 1);
+-- insert into contractors (contractor_id, contractor_email, password, first_name, last_name, managers_id, status) values (9, 'Mike@gmail.co.uk', 'Mike73', 'Mike', 'Jones', 4, 1);
+-- insert into contractors (contractor_id, contractor_email, password, first_name, last_name, managers_id, status) values (10, 'Jane@gmail.co.uk', 'Jane72!', 'Jane', 'Smith', 5, 0);
+-- insert into contractors (contractor_id, contractor_email, password, first_name, last_name, managers_id, status) values (11, 'Sarah@gmail.co.uk', 'Shar333', 'Sarah', 'Smith', 5, 1);
+-- insert into contractors (contractor_id, contractor_email, password, first_name, last_name, managers_id, status) values (12, 'Mike@gmail.co.uk', 'Mike62626', 'Mike', 'Zuch', 5, 1);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,57 +1,51 @@
 SET MODE MySQL;
 SET IGNORECASE = TRUE;
 
+
+
 -- -----------------------------------------------------
--- Table `admiral_staff`
+-- Table `admiral_user`
 -- -----------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS `admiral_staff`
+CREATE TABLE IF NOT EXISTS `admiral_user`
 (
-    `admiral_staff_id`              INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `admiral_staff_email`           VARCHAR(45)  NOT NULL,
-    `password`                      VARCHAR(45)  NOT NULL,
+    `admiral_user_id`               INT UNSIGNED NOT NULL AUTO_INCREMENT,
     `first_name`                    VARCHAR(45)  NOT NULL,
     `last_name`                     VARCHAR(45)  NOT NULL,
+    `admiral_user_email`            VARCHAR(45)  NOT NULL,
+    `password`                      VARCHAR(45)  NOT NULL,
+    `status`                        BOOLEAN  NOT NULL,
 
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`admiral_user_id`)
 )
     ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `managers`
+-- Table `admiral_role`
 -- -----------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS `managers`
+CREATE TABLE IF NOT EXISTS `admiral_role`
 (
-    `manager_id`                   INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `manager_email`                VARCHAR(45)  NOT NULL,
-    `password`                      VARCHAR(45)  NOT NULL,
-    `first_name`                    VARCHAR(45)  NOT NULL,
-    `last_name`                     VARCHAR(45)  NOT NULL,
-    `status`                        BIT  NOT NULL,
+    `admiral_role_id`                   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `admiral_role_name`                 VARCHAR(45)  NOT NULL,
+    `admiral_role_description`          VARCHAR(250)  NOT NULL,
 
 
-    PRIMARY KEY (`managers_id`)
+    PRIMARY KEY (`admiral_role_id`)
 )
     ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `contractors`
+-- Table `admiral_user_role`
 -- -----------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS `contractors`
+CREATE TABLE IF NOT EXISTS `admiral_user_role`
 (
-    `contractor_id`                   INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `contractor_email`                VARCHAR(45)  NOT NULL,
-    `password`                      VARCHAR(45)  NOT NULL,
-    `first_name`                    VARCHAR(45)  NOT NULL,
-    `last_name`                     VARCHAR(45)  NOT NULL,
-    `managers_id`                   INT  NOT NULL,
-    `status`                        BIT  NOT NULL,
+    `admiral_user_id`                   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `admiral_role_id`                   INT UNSIGNED NOT NULL AUTO_INCREMENT,
 
-
-    PRIMARY KEY (`contractor_id`)
-    FOREIGN KEY (`managers_id`) REFERENCES managers (`managers_id`)
+    FOREIGN KEY (`admiral_user_id`) REFERENCES admiral_user (`admiral_user_id`),
+    FOREIGN KEY (`admiral_role_id`) REFERENCES admiral_role (`admiral_role_id`)
 
 )
     ENGINE = InnoDB;
@@ -64,7 +58,7 @@ CREATE TABLE IF NOT EXISTS `contractors`
 CREATE TABLE IF NOT EXISTS `timesheet`
 (
     `timesheet_id`                   INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `contractor_id`                  INT  NOT NULL,
+    `admiral_role_id`                  INT  NOT NULL,
     `number_days`                    INT NOT NULL,
     `overtime`                       INT  NOT NULL,
     `start_date`                     DATE  NOT NULL,
@@ -74,8 +68,87 @@ CREATE TABLE IF NOT EXISTS `timesheet`
     `status`                         VARCHAR(25) NOT NULL,
 
 
-    PRIMARY KEY (`timesheet_id`)
-    FOREIGN KEY (`contractor_id`) REFERENCES contractors (`contractor_id`)
+    PRIMARY KEY (`timesheet_id`),
+    FOREIGN KEY (`admiral_role_id`) REFERENCES admiral_role (`admiral_role_id`)
 
 )
     ENGINE = InnoDB;
+
+-- -- -----------------------------------------------------
+-- -- Table `admiral_staff`
+-- -- -----------------------------------------------------
+--
+-- CREATE TABLE IF NOT EXISTS `admiral_staff`
+-- (
+--     `admiral_staff_id`              INT UNSIGNED NOT NULL AUTO_INCREMENT,
+--     `admiral_staff_email`           VARCHAR(45)  NOT NULL,
+--     `password`                      VARCHAR(45)  NOT NULL,
+--     `first_name`                    VARCHAR(45)  NOT NULL,
+--     `last_name`                     VARCHAR(45)  NOT NULL,
+--
+--     PRIMARY KEY (`id`)
+-- )
+--     ENGINE = InnoDB;
+--
+-- -- -----------------------------------------------------
+-- -- Table `managers`
+-- -- -----------------------------------------------------
+--
+-- CREATE TABLE IF NOT EXISTS `managers`
+-- (
+--     `manager_id`                   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+--     `manager_email`                VARCHAR(45)  NOT NULL,
+--     `password`                      VARCHAR(45)  NOT NULL,
+--     `first_name`                    VARCHAR(45)  NOT NULL,
+--     `last_name`                     VARCHAR(45)  NOT NULL,
+--     `status`                        BIT  NOT NULL,
+--
+--
+--     PRIMARY KEY (`managers_id`)
+-- )
+--     ENGINE = InnoDB;
+--
+-- -- -----------------------------------------------------
+-- -- Table `contractors`
+-- -- -----------------------------------------------------
+--
+-- CREATE TABLE IF NOT EXISTS `contractors`
+-- (
+--     `contractor_id`                   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+--     `contractor_email`                VARCHAR(45)  NOT NULL,
+--     `password`                      VARCHAR(45)  NOT NULL,
+--     `first_name`                    VARCHAR(45)  NOT NULL,
+--     `last_name`                     VARCHAR(45)  NOT NULL,
+--     `managers_id`                   INT  NOT NULL,
+--     `status`                        BIT  NOT NULL,
+--
+--
+--     PRIMARY KEY (`contractor_id`)
+--     FOREIGN KEY (`managers_id`) REFERENCES managers (`managers_id`)
+--
+-- )
+--     ENGINE = InnoDB;
+--
+--
+-- -- -----------------------------------------------------
+-- -- Table `time_sheets`
+-- -- -----------------------------------------------------
+--
+-- CREATE TABLE IF NOT EXISTS `timesheet`
+-- (
+--     `timesheet_id`                   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+--     `contractor_id`                  INT  NOT NULL,
+--     `number_days`                    INT NOT NULL,
+--     `overtime`                       INT  NOT NULL,
+--     `start_date`                     DATE  NOT NULL,
+--     `end_date`                       DATE  NOT NULL,
+--     `date_submitted`                 DATE  NOT NULL,
+--     `notes`                          VARCHAR(100),
+--     `status`                         VARCHAR(25) NOT NULL,
+--
+--
+--     PRIMARY KEY (`timesheet_id`)
+--     FOREIGN KEY (`contractor_id`) REFERENCES contractors (`contractor_id`)
+--
+-- )
+--     ENGINE = InnoDB;
