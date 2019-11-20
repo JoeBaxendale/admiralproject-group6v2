04011ce19e913559739c2747b879d48ec536780e
c1883096 <C1883096@cardiff.ac.uk>
2019-11-20 15:19:01 +0000

    Route mapping to controller

--- src/main/java/admiral/web/TimeSheetController.java
+++ src/main/java/admiral/web/TimeSheetController.java
@@ -1,4 +1,24 @@
 package admiral.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
 public class TimeSheetController {
+
+
+    public TimeSheetController() {
+        // for finders
+    }
+
+    @RequestMapping(path = "/Timesheet", method = RequestMethod.GET)
+    public String timeSheetDetails(Model model) {
+
+        return "t_time_sheet_details_page";
+
+    }
 }
