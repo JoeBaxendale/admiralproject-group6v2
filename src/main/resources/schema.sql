752e3de0e85f40ff0dc2f430d206d81deb82bc93
Karla Mendivelso Espinosa <MendivelsoEspinosaKN@cardiff.ac.uk>
2019-11-21 12:35:36 +0000

    Added new schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,52 +4,69 @@ SET IGNORECASE = TRUE;
 
 
 -- -----------------------------------------------------
--- Table `admiral_user`
+-- Table `users`
 -- -----------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS `admiral_user`
+CREATE TABLE IF NOT EXISTS `users`
 (
-    `admiral_user_id`               INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `user_id`                       INT UNSIGNED NOT NULL AUTO_INCREMENT,
     `first_name`                    VARCHAR(45)  NOT NULL,
     `last_name`                     VARCHAR(45)  NOT NULL,
-    `admiral_user_email`            VARCHAR(45)  NOT NULL,
+    `email`                         VARCHAR(45)  NOT NULL,
     `password`                      VARCHAR(45)  NOT NULL,
-    `status`                        BOOLEAN  NOT NULL,
+    `role_id`                       INT NOT NULL,
+    `active`                        BIT NOT NULL,
 
-    PRIMARY KEY (`admiral_user_id`)
+    PRIMARY KEY (`user_id`)
+    FOREIGN KEY (`role_id`) REFERENCES roles (`role_id`)
 )
     ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `admiral_role`
+-- Table `roles`
 -- -----------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS `admiral_role`
+CREATE TABLE IF NOT EXISTS `roles`
 (
-    `admiral_role_id`                   INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `admiral_role_name`                 VARCHAR(45)  NOT NULL,
-    `admiral_role_description`          VARCHAR(250)  NOT NULL,
+    `role_id`                   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `role_name`                 VARCHAR(45)  NOT NULL,
+    `role_description`          VARCHAR(250) NOT NULL,
 
-
-    PRIMARY KEY (`admiral_role_id`)
+    PRIMARY KEY (`role_id`)
 )
     ENGINE = InnoDB;
 
+
 -- -----------------------------------------------------
--- Table `admiral_user_role`
+-- Table `managers`
 -- -----------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS `admiral_user_role`
+CREATE TABLE IF NOT EXISTS `managers`
 (
-    `admiral_user_id`                   INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `admiral_role_id`                   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `manager_id`             INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `user_id`                INT UNSIGNED NOT NULL,
 
-    FOREIGN KEY (`admiral_user_id`) REFERENCES admiral_user (`admiral_user_id`),
-    FOREIGN KEY (`admiral_role_id`) REFERENCES admiral_role (`admiral_role_id`)
-
+    PRIMARY KEY (`manager_id`)
+    FOREIGN KEY (`user_id`) REFERENCES users (`user_id`)
 )
     ENGINE = InnoDB;
 
+-- -----------------------------------------------------
+-- Table `contractors`
+-- -----------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `contractors`
+(
+    `contractor_id`          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `user_id`                INT UNSIGNED NOT NULL,
+    `manager_id`             INT UNSIGNED NOT NULL,
+
+
+    PRIMARY KEY (`contractor_id`)
+    FOREIGN KEY (`user_id`) REFERENCES users (`user_id`)
+    FOREIGN KEY (`manager_id`) REFERENCES managers (`manager_id`)
+)
+    ENGINE = InnoDB;
 
 -- -----------------------------------------------------
 -- Table `time_sheets`
@@ -58,97 +75,17 @@ CREATE TABLE IF NOT EXISTS `admiral_user_role`
 CREATE TABLE IF NOT EXISTS `timesheet`
 (
     `timesheet_id`                   INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `admiral_role_id`                  INT  NOT NULL,
+    `contractor_id`                  INT NOT NULL,
     `number_days`                    INT NOT NULL,
-    `overtime`                       INT  NOT NULL,
-    `start_date`                     DATE  NOT NULL,
-    `end_date`                       DATE  NOT NULL,
-    `date_submitted`                 DATE  NOT NULL,
+    `overtime`                       INT NOT NULL,
+    `start_date`                     DATE NOT NULL,
+    `end_date`                       DATE NOT NULL,
+    `date_submitted`                 DATE NOT NULL,
     `notes`                          VARCHAR(100),
     `status`                         VARCHAR(25) NOT NULL,
 
-
     PRIMARY KEY (`timesheet_id`),
-    FOREIGN KEY (`admiral_role_id`) REFERENCES admiral_role (`admiral_role_id`)
+    FOREIGN KEY (`contractor_id`) REFERENCES contractors (`contractor_id`)
 
 )
     ENGINE = InnoDB;
-
--- -- -----------------------------------------------------
--- -- Table `admiral_staff`
--- -- -----------------------------------------------------
---
--- CREATE TABLE IF NOT EXISTS `admiral_staff`
--- (
---     `admiral_staff_id`              INT UNSIGNED NOT NULL AUTO_INCREMENT,
---     `admiral_staff_email`           VARCHAR(45)  NOT NULL,
---     `password`                      VARCHAR(45)  NOT NULL,
---     `first_name`                    VARCHAR(45)  NOT NULL,
---     `last_name`                     VARCHAR(45)  NOT NULL,
---
---     PRIMARY KEY (`id`)
--- )
---     ENGINE = InnoDB;
---
--- -- -----------------------------------------------------
--- -- Table `managers`
--- -- -----------------------------------------------------
---
--- CREATE TABLE IF NOT EXISTS `managers`
--- (
---     `manager_id`                   INT UNSIGNED NOT NULL AUTO_INCREMENT,
---     `manager_email`                VARCHAR(45)  NOT NULL,
---     `password`                      VARCHAR(45)  NOT NULL,
---     `first_name`                    VARCHAR(45)  NOT NULL,
---     `last_name`                     VARCHAR(45)  NOT NULL,
---     `status`                        BIT  NOT NULL,
---
---
---     PRIMARY KEY (`managers_id`)
--- )
---     ENGINE = InnoDB;
---
--- -- -----------------------------------------------------
--- -- Table `contractors`
--- -- -----------------------------------------------------
---
--- CREATE TABLE IF NOT EXISTS `contractors`
--- (
---     `contractor_id`                   INT UNSIGNED NOT NULL AUTO_INCREMENT,
---     `contractor_email`                VARCHAR(45)  NOT NULL,
---     `password`                      VARCHAR(45)  NOT NULL,
---     `first_name`                    VARCHAR(45)  NOT NULL,
---     `last_name`                     VARCHAR(45)  NOT NULL,
---     `managers_id`                   INT  NOT NULL,
---     `status`                        BIT  NOT NULL,
---
---
---     PRIMARY KEY (`contractor_id`)
---     FOREIGN KEY (`managers_id`) REFERENCES managers (`managers_id`)
---
--- )
---     ENGINE = InnoDB;
---
---
--- -- -----------------------------------------------------
--- -- Table `time_sheets`
--- -- -----------------------------------------------------
---
--- CREATE TABLE IF NOT EXISTS `timesheet`
--- (
---     `timesheet_id`                   INT UNSIGNED NOT NULL AUTO_INCREMENT,
---     `contractor_id`                  INT  NOT NULL,
---     `number_days`                    INT NOT NULL,
---     `overtime`                       INT  NOT NULL,
---     `start_date`                     DATE  NOT NULL,
---     `end_date`                       DATE  NOT NULL,
---     `date_submitted`                 DATE  NOT NULL,
---     `notes`                          VARCHAR(100),
---     `status`                         VARCHAR(25) NOT NULL,
---
---
---     PRIMARY KEY (`timesheet_id`)
---     FOREIGN KEY (`contractor_id`) REFERENCES contractors (`contractor_id`)
---
--- )
---     ENGINE = InnoDB;
