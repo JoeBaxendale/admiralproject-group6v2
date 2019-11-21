02a6cc927ea3b41983853534a490c4f84ceb3bc2
Karla Mendivelso Espinosa <MendivelsoEspinosaKN@cardiff.ac.uk>
2019-11-21 20:00:57 +0000

    Edited build.gradle dependencies

--- build/resources/main/schema.sql
+++ build/resources/main/schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS `users`
     `first_name`                    VARCHAR(45)  NOT NULL,
     `last_name`                     VARCHAR(45)  NOT NULL,
     `email`                         VARCHAR(45)  NOT NULL,
-    `password`                      VARCHAR(45)  NOT NULL,
+    `password`                      VARCHAR(70)  NOT NULL,
     `role_id`                       INT NOT NULL,
     `active`                        BIT NOT NULL,
 
