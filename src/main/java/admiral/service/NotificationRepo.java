14207b8ba34900a905e5639695f357ee0b58ccad
Dexter Gordon <GordonD2@cardiff.ac.uk>
2019-12-05 01:18:37 +0000

    New repository interface to support obtaining information for notifications

--- src/main/java/admiral/service/NotificationRepo.java
+++ src/main/java/admiral/service/NotificationRepo.java
@@ -1,4 +1,15 @@
 package admiral.service;
 
+import admiral.domain.NotificationDetails;
+
+//----------------------------------------------------------------------------------------------------------------------
+// Interface for the timesheet Notification repository, including all methods required to obtain the information
+// needed to support timesheet notifications
 public interface NotificationRepo {
+
+    //------------------------------------------------------------------------------------------------------------------
+    // Method to retrieve notification information for a specified timesheet identity
+    // The notification details will require obtaining information from multiple tables
+    public NotificationDetails findNotificationDetailsById(int time_sheet_id);
+
 }
