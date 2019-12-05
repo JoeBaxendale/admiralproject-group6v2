da6328ebb3dc8adefec0bc674d0ee3160223607f
Dexter Gordon <GordonD2@cardiff.ac.uk>
2019-12-05 01:22:15 +0000

    Interface for a service to handle sending notification

--- src/main/java/admiral/service/NotificationInterface.java
+++ src/main/java/admiral/service/NotificationInterface.java
@@ -1,4 +1,11 @@
 package admiral.service;
 
+//------------------------------------------------------------------------------------------------------------------
+// Interface for timesheet notifications. It is intended that notification will be sent to Microsoft teams.
 public interface NotificationInterface {
+
+    //------------------------------------------------------------------------------------------------------------------
+    // Method to send a notification to Microsoft Teams when the specified Time Sheet has been created.
+    // The method implementation wil determine how this is sent (e.g. using Teams webhook)
+    public void sendTimeSheetCreateNotification(int time_sheet_id);
 }
